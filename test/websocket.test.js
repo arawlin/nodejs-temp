8475f069b016c1938090c104fcdaeeb7ef5022c3
snakewarhead <snakewarhead@gmail.com>
2023-07-08 21:40:26 +0800

    websocket, reconnect 2

--- test/websocket.test.js
+++ test/websocket.test.js
@@ -2,8 +2,9 @@ const WS = require('../lib/websocket')
 const log = require('../lib/logger').init('websocket-test')
 
 let count = 0
+let disconnected = false
 
-const ws = new WS('ws://localhost:1010', { retryCount: 5, retryInterval: 3000 }, (event) => {
+const ws = new WS('ws://localhost:1010', { retryCount: 2, retryInterval: 3000 }, (event) => {
   log.info(event.data, event.type)
 })
 log.info(ws.url, ws.options)
@@ -12,21 +13,29 @@ ws.on('connect', () => {
   log.info('connect wsr')
 })
 ws.on('disconnect', () => {
+  disconnected = true
   log.info('disconnect wsr')
 })
 ws.on('reconnect', () => {
-  log.info('reconnect wsr')
+  log.info('reconnect wsr', ++count)
 })
 ws.on('open', (event) => {
   log.info('open wsr', event.type)
 })
 ws.on('error', (event) => {
-  log.error(count++, 'error wsr', event.type)
+  log.error('error wsr!!!!!!!!!', event.type)
 })
 ws.on('close', (event) => {
   log.info('close wsr', event.type)
 })
 
+const intervalID = setInterval(() => {
+  ws.isConnected && ws.send(count)
+  if (disconnected) {
+    clearInterval(intervalID)
+  }
+}, 1 * 1000)
+
 setTimeout(() => {
   ws.disconnect()
-}, 10 * 1000)
+}, 100 * 1000)
--- test/websocketserver.test.js
+++ test/websocketserver.test.js
@@ -2,17 +2,24 @@ const WebSocket = require('ws')
 
 const wss = new WebSocket.Server({ port: 1010 })
 
-wss.on('connection', function connection(ws) {
-  console.log('new connection')
+wss.on('connection', function (ws) {
+  console.log('new connection', ws instanceof WebSocket, Object.getOwnPropertyNames(ws))
+
   ws.on('message', function incoming(data) {
     console.log('incoming message: ' + data)
   })
 
+  ws.on('close', function () {
+    console.log('client socket closed')
+  })
+
   setInterval(function () {
-    ws.close()
+    ws?.close()
   }, 10000)
 
   setInterval(function () {
     if (ws.readyState === 1) ws.send('hello')
   }, 1000)
 })
+
+console.log('listening ------------ ')
