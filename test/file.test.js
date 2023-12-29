c688b783363d634efe8e149bc9616abb454e56fe
snakewarhead <snakewarhead@gmail.com>
2023-12-29 17:13:42 +0800

    lib file update

--- test/file.test.js
+++ test/file.test.js
@@ -2,14 +2,20 @@ const fs = require('../lib/file')
 
 describe('file', () => {
   it('mkdir', async () => {
-    await fs.mkdir('aa', 'bb', 'cc')
+    await fs.mkdir('logs', 'bb', 'cc')
+  })
+
+  it('writ', async () => {
+    await fs.writeLine('logs/bb/cc/a.a', 'aaa')
+    await fs.writeLine('logs/bb/cc/a.a', 'bbb')
+    await fs.writeLine('logs/bb/cc/a.a', 'ccc')
   })
 
   it('read', async () => {
-    const ct = await fs.read('aa/bb/cc/a.a')
+    const ct = await fs.read('logs/bb/cc/a.a')
     console.log(ct)
 
-    const lines = await fs.readLine('aa/bb/cc/a.a')
+    const lines = await fs.readLines('logs/bb/cc/a.a')
     console.log(lines)
   })
 })
